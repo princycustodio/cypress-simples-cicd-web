dcad1805e6595b9a26692888e873f07ad53839d2
princycustodio <princyalexcustodio@gmail.com>
2026-05-13 17:32:40 -0300

    correção do import2

--- cypress/e2e/cadastroProdutos.cy.js
+++ cypress/e2e/cadastroProdutos.cy.js
@@ -5,7 +5,7 @@ describe('Cadastro de Produtos', () => {
 
   beforeEach(() => {
 
-    cadastroProdutoPage.acessarSite()
+    CadastroProdutoPage.acessarSite()
 
   })
 
@@ -16,15 +16,15 @@ describe('Cadastro de Produtos', () => {
     const preco = '4500'
     const descricao = 'Notebook RTX 4060'
 
-    cadastroProdutoPage.clicarAdicionarProduto()
+    CadastroProdutoPage.clicarAdicionarProduto()
 
-    cadastroProdutoPage.preencherNome(produto)
+    CadastroProdutoPage.preencherNome(produto)
 
-    cadastroProdutoPage.preencherPreco(preco)
+    CadastroProdutoPage.preencherPreco(preco)
 
-    cadastroProdutoPage.preencherDescricao(descricao)
+    CadastroProdutoPage.preencherDescricao(descricao)
 
-    cadastroProdutoPage.clicarSalvar()
+    CadastroProdutoPage.clicarSalvar()
 
     // Validação final
     cy.contains(produto).should('be.visible')
@@ -40,37 +40,36 @@ describe('Cadastro de Produtos', () => {
     const precoOriginal = '150'
     const descricaoOriginal = 'Mouse RGB'
 
-    cadastroProdutoPage.clicarAdicionarProduto()
+    CadastroProdutoPage.clicarAdicionarProduto()
 
-    cadastroProdutoPage.preencherNome(produtoOriginal)
+    CadastroProdutoPage.preencherNome(produtoOriginal)
 
-    cadastroProdutoPage.preencherPreco(precoOriginal)
+    CadastroProdutoPage.preencherPreco(precoOriginal)
 
-    cadastroProdutoPage.preencherDescricao(descricaoOriginal)
+    CadastroProdutoPage.preencherDescricao(descricaoOriginal)
 
-    cadastroProdutoPage.clicarSalvar()
+    CadastroProdutoPage.clicarSalvar()
 
     // Editar produto
-
-    cadastroProdutoPage.clicarEditar(produtoOriginal)
+    CadastroProdutoPage.clicarEditar(produtoOriginal)
 
     const novoProduto = 'PRO' + produtoOriginal
     const novoPreco = '250'
     const novaDescricao = 'Mouse RGB PRO'
 
-    cy.get(cadastroProdutoPage.elements.nomeInput)
+    cy.get(CadastroProdutoPage.elements.nomeInput)
       .clear()
       .type(novoProduto)
 
-    cy.get(cadastroProdutoPage.elements.precoInput)
+    cy.get(CadastroProdutoPage.elements.precoInput)
       .clear()
       .type(novoPreco)
 
-    cy.get(cadastroProdutoPage.elements.descricaoInput)
+    cy.get(CadastroProdutoPage.elements.descricaoInput)
       .clear()
       .type(novaDescricao)
 
-    cadastroProdutoPage.clicarSalvar()
+    CadastroProdutoPage.clicarSalvar()
 
     // Validação final
     cy.contains(novoProduto).should('be.visible')
@@ -85,21 +84,21 @@ describe('Cadastro de Produtos', () => {
     const preco = '300'
     const descricao = 'Switch Blue'
 
-    cadastroProdutoPage.clicarAdicionarProduto()
+    CadastroProdutoPage.clicarAdicionarProduto()
 
-    cadastroProdutoPage.preencherNome(produto)
+    CadastroProdutoPage.preencherNome(produto)
 
-    cadastroProdutoPage.preencherPreco(preco)
+    CadastroProdutoPage.preencherPreco(preco)
 
-    cadastroProdutoPage.preencherDescricao(descricao)
+    CadastroProdutoPage.preencherDescricao(descricao)
 
-    cadastroProdutoPage.clicarSalvar()
+    CadastroProdutoPage.clicarSalvar()
 
     // Validar cadastro
     cy.contains(produto).should('be.visible')
 
     // Excluir produto
-    cadastroProdutoPage.clicarExcluir(produto)
+    CadastroProdutoPage.clicarExcluir(produto)
 
     // Validação final
     cy.contains(produto).should('not.exist')
